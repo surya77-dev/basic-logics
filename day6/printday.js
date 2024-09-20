23f8489351fc6071fbf5edc2740de3afa667fa67
surya77-dev <suryavln77@gmail.com>
2024-09-21 04:02:18 +0530

    day6 push

---
+++ day6/printday.js
@@ -0,0 +1,4 @@
+let days = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday'];
+for(let i = 0; i<days.length; i++){
+  console.log(days[i]);
+}
---
+++ day6/printnum.js
@@ -0,0 +1,5 @@
+let array = [1,2,3,4,5,6,7,8,9,10];
+let num = 10;
+for(let i = 0; i<num ; i++){
+  console.log(array[i]); 
+}
